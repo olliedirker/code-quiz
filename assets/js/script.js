82dc31ebbc42be2094b33c42c8e91b41dfded110
olliedirker <olliedirker@gmail.com>
2022-06-01 17:53:19 -0500

    html and also start button

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function startQuiz(){
+question()
+}
+
+
+var startBtn = document.querySelector("#startButton")
+startBtn.addEventListener("click", startQuiz);
+var answer = document.querySelector("#quizPage")
+//nextQuestion execute the hide and reveal animations
+answer.addEventListener("click", nextQuestion)
+
+
+
+function question(){
+
+    if(("#correct")){
+
+    }
+    
+}
+
+
